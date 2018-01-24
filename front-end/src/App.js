ef32f2da526c41ab6d4fa91ee9916dc88f1ece30
Danny Warren <dannydwarren@gmail.com>
2018-01-23 19:25:39 -0700

    Add cars to front-end

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,27 +4,41 @@ import './App.css';
 
 class App extends Component {
   state = {
-    message: "NO MESSAGE RECEIVED"
+    cars: []
   }
 
-  getData = async () => {
-    const url = "http://localhost:3001"
+  getData = async (path) => {
+    const url = `http://localhost:3001${path}`
     const response = await fetch(url)
     const data = await response.json()
 
     return data
   }
 
+  displayCars = (cars) => {
+    const carElements = []
+    for (const car of cars) {
+      carElements.push(
+      <div>
+        <h1>{car.make}</h1>
+        <h2>{car.model}</h2>
+        <h3>{car.year}</h3>
+        <h4>{car.mileage}</h4>
+      </div>)
+    }
+    return carElements
+  }
+
   async componentDidMount() {
-    const data = await this.getData()
-    console.log("ServerData:", data)
-    this.setState({ message: data.message })
+    const carsResponse = await this.getData("/cars")
+    console.log("ServerData:", carsResponse)
+    this.setState({ cars: carsResponse.cars })
   }
 
   render() {
     return (
       <div className="App">
-        <h1>{this.state.message}</h1>
+        {this.displayCars(this.state.cars)}
       </div>
     );
   }
